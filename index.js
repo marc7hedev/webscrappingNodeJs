eac60fe7c61a3be43c376e01dea5d2c21c46caec
marc7hedev <driberstagg@gmail.com>
2024-07-31 23:12:19 -0600

    Añadida función para extraer datos y cambios de ventana automaticos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
+const xlsx = require("xlsx");
 
 (async () => {
 
-    const URL = "https://www.amazon.com/s?k=PROGRAMMER+SOCKS&crid=23NDX4OY0C0YC&sprefix=programmer+socks%2Caps%2C134&ref=nb_sb_noss_1";
+    // const URL = "https://www.amazon.com/s?k=programming+socks&crid=2N7PNBPAGV4RV&sprefix=progrramming+s%2Caps%2C271&ref=nb_sb_ss_sc_1_13";
+    const URL = "https://www.amazon.com/";
 
     const browser = await puppeteer.launch({
         headless: false
@@ -17,10 +19,52 @@ const puppeteer = require('puppeteer');
 
     let products = [];
     let nextPage = true;
+
     while (nextPage){
-        await page.evaluate(() => {
-            document.querySelectorAll('.puis-card-container');
+        const newProducts = await page.evaluate(() => {
+            const products = Array.from(document.querySelectorAll('.puis-card-container.s-card-container'));
+
+            return products.map(product => {
+                const title = product.querySelector('.a-text-normal')?.innerText;
+                const priceWhole = product.querySelector('.a-price-whole')?.innerText;
+                const priceFraction = product.querySelector('.a-price-fraction')?.innerText;
+
+                if (!priceWhole || !priceFraction) {
+                    return
+                }
+
+                const priceWholeCleaned = priceWhole.replace(/\n/g, "").trim();
+                const priceFractionCleaned = priceFraction.replace(/\n/g, "").trim();
+
+                return {
+                    title,
+                    price: `${priceWhole}.${priceFraction}`
+                }
+
+            });
         })
+
+        products = [...products, ...newProducts]
+
+        nextPage = await page.evaluate(() => {
+            const nextButton = document.querySelector('.s-pagination-next');
+            
+            if(nextButton && 
+                !nextButton.classList.contains('s-pagination-disabled')
+            ){
+                nextButton.click();
+                return true;
+            }
+
+            return false;
+
+        })
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+
     }
+
+    console.log(products);
 
 })();
